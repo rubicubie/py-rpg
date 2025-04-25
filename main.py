8f1d5e44b473a85c04034178e2de9b40dee3cf4b
rubicubie <omenonem@gmail.com>
2025-04-25 11:29:37 -0300

    VERY UNFINISHED!!! CODE DOESNT WORK

--- main.py
+++ main.py
@@ -1,37 +1,75 @@
 import random
 import os
 
+char = None
+
 def clear():
     os.system('cls')
 
 # Characters to select
-char = 0
-
-# Basic Character
-base = {
-    "hp": 10,
-    "maxhp": 10,
-    "mp": 10,
-    "maxmp": 10,
-    "crit": 1 # Gonna make crits later
-}
+class player:
+    def __init__(self, hp, maxhp, mp, maxmp, crit):
+        self.hp = hp
+        self.maxhp = maxhp
+        self.mp = mp
+        self.maxmp = maxmp
+        self.crit = crit
 
 def charselect():
+    clear()
     print("Select a character:")
     print("[1] - Basic Character - 10HP 10MP")
+    print("[0] - Back to Menu")
 
-    char = input("> ")
-    if char == 1:
-        char = "base"
+    select = int(input("> "))
+    if select == 1:
+        char = player(10,10,10,10,2)
+        print("CHARACTER CHANGED")
+    elif select == 0:   
+        clear()
+        gamemenu()
 
 def stats():
+    clear()
+    print(char)
     print("- Character Status - ")
-    print("HP:", char.get("hp"), "/", char.get("maxhp"))
-    print("MP:", char.get("mp"), "/", char.get("maxmp"))
+    print("HP:", char.hp, "/", char.maxhp)
+    print("MP:", char.mp, "/", char.maxmp)
 
+def gamemenu():
+    print("- Python RPG -\n")
+    print("[1] - START")
+    print("[2] - ABOUT")
+    print("[3] - EXIT")
+    menu = int(input("> "))
+    print(menu)
 
-# Game Start
-print("- The RPG with barely no RNG, trust me -\n")
-print("[1] - START")
-print("[2] - ABOUT")
-print("[3] - EXIT")
+    if menu == 1 :
+        clear()
+        start()
+    elif menu == 2:
+        clear()
+        about()
+    else:
+        clear()
+        print("Bye!")
+
+def about():
+    clear()
+    print("This game was made while having programming classes")
+    print("Python was a language i got quickly used to! :D")
+    print("I hope you enjoy a bit of this game, the same way")
+    print("i've enjoyed making it! :3\n")
+    input("> ")
+    gamemenu()
+
+# Main function to move the game
+def start():
+    charselect()
+
+gamemenu()
+end = input("> ")
+stats()
+
+# Just so the game doesn't disappear after ending
+end = input("> ")
